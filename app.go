3b818aa620b4f3a95128de8acb2ce0ae03965c7d
Pedro Gabriel <pedrogabriel@protonmail.ch>
2018-11-03 10:52:24 +0000

    Start app. Config first routes. Seed questions into memory.

---
+++ app.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+var questions []Question
+
+type App struct {
+	Router *mux.Router
+}
+
+func seedQuestions() {
+	questions = make([]Question, 3)
+	questions[0] = Question{
+		Content:    "What is the national animal of China?",
+		corrOption: "Giant Panda",
+		Options:    [4]string{"Giant Panda", "Panda", "Red Panda", "Polar Bear"},
+	}
+	questions[1] = Question{
+		Content:    "What is the noisiest city in the world?",
+		corrOption: "Hong Kong",
+		Options:    [4]string{"Lisbon", "London", "New York", "Hong Kong"},
+	}
+	questions[2] = Question{
+		Content:    "What is a very cold part of Russia?",
+		corrOption: "Siberia",
+		Options:    [4]string{"Magadan", "Krasnoyarsk", "Siberia", "Moscow"},
+	}
+}
+
+// Run func should be used to start the server
+func (a *App) Run(addr string) {
+	a.initialize()
+	log.Fatal(http.ListenAndServe(addr, a.Router))
+}
+
+func (a *App) initialize() {
+	a.Router = mux.NewRouter()
+
+	a.Router.HandleFunc("/quiz", getQuiz).Methods("GET")
+	a.Router.HandleFunc("/quiz/results", saveResults).Methods("POST")
+
+	seedQuestions()
+}
+
+func getQuiz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(questions); err != nil {
+		panic(err)
+	}
+
+}
+
+func saveResults(w http.ResponseWriter, r *http.Request) {
+
+}
---
+++ question.go
@@ -0,0 +1,8 @@
+package main
+
+// Question with a set of options
+type Question struct {
+	Content    string    `json:"content"`
+	Options    [4]string `json:"options"`
+	corrOption string
+}
